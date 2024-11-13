491dd0929203514c30b36f4e9341e6b70cf30906
Yaa-Henewaa <maameyaahenewaa2001@gmail.com>
2024-11-12 15:07:01 -1200

    re-verting revert

--- src/api/models/user.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const UserSchema = new mongoose.Schema({
-  // FirstName, lastName, email,password
-});
-
-const User = new mongoose.Model("User", UserSchema);
-export default User;
