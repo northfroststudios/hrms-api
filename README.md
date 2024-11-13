5a297019fe0f91bafa1c7ba68f724d050690e31b
Yaa-Henewaa <maameyaahenewaa2001@gmail.com>
2024-11-12 14:00:13 -1200

    Revert "register and login"
    This reverts commit d844f697cb7180ae10b6fda3d0138ffdb97cf254.

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # hrms-api
 HRMS API
-
-
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,11 @@
 import express from 'express';
-import userRoutes from './Routes/userRoutes'
-import './Db/db'
-import dotenv from 'dotenv';
-dotenv.config();
-
 
 const app = express();
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
-app.use('/api/users', userRoutes)
 const port = process.env.PORT || 3000;
 
-
+app.get('/', (req, res) => {
+  res.send('Hello, TypeScript with Node.js and Express!');
+});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
